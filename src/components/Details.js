1cff79e6698f521be6c8fe23b6c5fe71a1989840
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-15 19:19:11 -0400

    Will now load the data from the database into the fields on the details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -24,11 +24,11 @@ const Details = () => {
   return (
     <Container>
       <Background>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg" />
+        <img alt={detailData.title} src={detailData.backgroundImg} />
       </Background>
 
       <ImageTitle>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5C647DF3FFBFA343CFEA84AC715148F25F9E86F398B408010CC403E7654FB908/scale?width=1440&aspectRatio=1.78" />
+        <img alt={detailData.title} src={detailData.titleImg} />
       </ImageTitle>
 
       <ContentMeta>
@@ -50,8 +50,8 @@ const Details = () => {
             </div>
           </GroupWatch>
         </Controls>
-        <SubTitle>SubTitle</SubTitle>
-        <Description>Description</Description>
+        <SubTitle>{detailData.subTitle}</SubTitle>
+        <Description>{detailData.description}</Description>
       </ContentMeta>
     </Container>
   );
