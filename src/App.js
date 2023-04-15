02249ed1adfc06b82d2ac02b65ff28ac58101e31
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-15 18:33:33 -0400

    Fixed typo resulting in routing error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Details from "./components/Details";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/home" element={[<Home />]} />
+          <Route path="/detail/:id" element={[<Details />]} />
           <Route path="/" element={[<Login />]} />
         </Routes>
       </Router>
---
+++ src/components/Details.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Details = () => {
+  return <Container>Detail!</Container>;
+};
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+`;
+
+export default Details;
