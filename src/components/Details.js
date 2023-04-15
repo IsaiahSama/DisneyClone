9133bd4b456121b3dfd510ea13047ec8745b2d5b
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-04-15 18:59:56 -0400

    Added Trailer, AddList and GroupWatch 'buttons'

--- src/components/Details.js
+++ src/components/Details.js
@@ -18,6 +18,18 @@ const Details = () => {
             <img src="/images/play-icon-black.png" />
             <span>Play</span>
           </Player>
+          <Trailer>
+            <img src="/images/play-icon-white.png" />
+            <span>Trailer</span>
+          </Trailer>
+          <AddList>
+            <span /> <span />
+          </AddList>
+          <GroupWatch>
+            <div>
+              <img src="/images/group-icon.png" />
+            </div>
+          </GroupWatch>
         </Controls>
       </ContentMeta>
     </Container>
@@ -112,6 +124,64 @@ const Player = styled.button`
     img {
       width: 25px;
     }
+  }
+`;
+
+const Trailer = styled(Player)`
+  background: rgba(0, 0, 0, 0.6);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+  margin-right: 16px;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 50%;
+  border: 2px solid white;
+  cursor: pointer;
+
+  span {
+    background-color: rgb(249, 249, 249);
+    display: inline-block;
+
+    &:first-child {
+      height: 2px;
+      transform: translate(1px, 0px) rotate(0deg);
+      width: 16px;
+    }
+
+    &:nth-child(2) {
+      height: 16px;
+      transform: translateX(-8px) rotate(0deg);
+      width: 2px;
+    }
+  }
+`;
+
+const GroupWatch = styled.div`
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background-color: white;
+
+  div {
+    height: 40px;
+    width: 40px;
+    background-color: rgb(0, 0, 0);
+    border-radius: 50%;
+  }
+
+  img {
+    width: 100%;
   }
 `;
 
